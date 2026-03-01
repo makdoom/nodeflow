1590b2ba310f9be75af8757c8d819363ac4987a4
Makdoom Shaikh <makshaikh99@gmail.com>
2026-03-01 11:45:56 +0530

    feat: Setup trpc

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/features/auth/components/login-form";
+
+const Page = () => {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <LoginForm />
+    </div>
+  );
+};
+export default Page;
---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,11 @@
+import { baseProcedure, createTRPCRouter } from "../init";
+
+export const appRouter = createTRPCRouter({
+  hello: baseProcedure.query(() => {
+    return {
+      greeting: `hello world`,
+    };
+  }),
+});
+
+export type AppRouter = typeof appRouter;
