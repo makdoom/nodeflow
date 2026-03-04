d909c522502710905a3191eba61fa5525b805a97
Makdoom Shaikh <makshaikh99@gmail.com>
2026-03-04 06:30:30 +0530

    feat: Implement workfolws page

--- src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
+++ src/app/(dashboard)/(editor)/workflows/[workflowId]/page.tsx
@@ -1,4 +1,14 @@
+import {
+  EditorLoading,
+  EditorError,
+  Editor,
+} from "@/features/editor/components/editor";
+import EditorHeader from "@/features/editor/components/editor-header";
+import { prefetchWorflow } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 interface PageProps {
   params: Promise<{ workflowId: string }>;
@@ -8,6 +18,19 @@ const Page = async ({ params }: PageProps) => {
   await requireAuth();
 
   const { workflowId } = await params;
-  return <div>workflowId - {workflowId} </div>;
+  prefetchWorflow(workflowId);
+
+  return (
+    <HydrateClient>
+      <ErrorBoundary fallback={<EditorError />}>
+        <Suspense fallback={<EditorLoading />}>
+          <EditorHeader workflowId={workflowId} />
+          <main className="flex-1">
+            <Editor workflowId={workflowId} />
+          </main>
+        </Suspense>
+      </ErrorBoundary>
+    </HydrateClient>
+  );
 };
 export default Page;
---
+++ src/features/editor/components/editor-header.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import { ModeToggle } from "@/components/dark-mode-toggle";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  useSuspenseWorkflow,
+  useUpdateWorkflowName,
+} from "@/features/workflows/hooks/use-workflows";
+import Link from "next/link";
+import { KeyboardEvent, useEffect, useRef, useState } from "react";
+import { IoSaveOutline } from "react-icons/io5";
+
+const EditorSaveButton = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <Button size="sm" onClick={() => {}} disabled={false}>
+      <IoSaveOutline />
+      Save
+    </Button>
+  );
+};
+
+const EditorNameInput = ({ workflowId }: { workflowId: string }) => {
+  const { data: workflow } = useSuspenseWorkflow(workflowId);
+  const updateWorkflowName = useUpdateWorkflowName();
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(workflow.name);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSave = async () => {
+    if (name == workflow.name) {
+      setIsEditing(false);
+      return;
+    }
+
+    try {
+      await updateWorkflowName.mutateAsync({ id: workflow.id, name });
+    } catch (error) {
+      setName(workflow.name);
+    } finally {
+      setIsEditing(false);
+    }
+  };
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key == "Enter") {
+      handleSave();
+    } else if (e.key == "Escape") {
+      setName(workflow.name);
+      setIsEditing(false);
+    }
+  };
+
+  useEffect(() => {
+    if (workflow) setName(workflow.name);
+  }, [workflow.name]);
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus();
+      inputRef.current.select();
+    }
+  }, [isEditing]);
+
+  if (isEditing) {
+    return (
+      <Input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        onBlur={handleSave}
+        onKeyDown={handleKeyDown}
+        disabled={updateWorkflowName.isPending}
+        className="h-7 w-auto min-w-25 px-2"
+      />
+    );
+  }
+
+  return (
+    <BreadcrumbItem
+      onClick={() => setIsEditing(true)}
+      className="cursor-pointer hover:text-foreground transition-colors"
+    >
+      {workflow.name}
+    </BreadcrumbItem>
+  );
+};
+const EditorBreadcrumb = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="/workflows">Workflows</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+
+        <EditorNameInput workflowId={workflowId} />
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+const EditorHeader = ({ workflowId }: { workflowId: string }) => {
+  return (
+    <header className="flex h-14 w-full shrink-0 border-b gap-2 bg-background px-4 items-center justify-between">
+      <div className="flex items-center gap-x-4 w-full">
+        <SidebarTrigger />
+        <div className="flex items-center justify-between w-full gap-x-4">
+          <EditorBreadcrumb workflowId={workflowId} />
+          <EditorSaveButton workflowId={workflowId} />
+        </div>
+      </div>
+      <ModeToggle />
+    </header>
+  );
+};
+
+export default EditorHeader;
