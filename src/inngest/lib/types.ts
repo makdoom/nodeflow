9ea900edc145f03a084feaec41d35cd0ca128a59
Makdoom Shaikh <makshaikh99@gmail.com>
2026-03-08 14:04:16 +0530

    feat: Add node exection feature

---
+++ src/inngest/lib/types.ts
@@ -0,0 +1,15 @@
+import type { GetStepTools, Inngest } from "inngest";
+
+export type WorkflowContext = Record<string, unknown>;
+export type StepTools = GetStepTools<Inngest.Any>;
+
+export type NodeExecutionParams<TData = Record<string, unknown>> = {
+  data: TData;
+  nodeId: string;
+  context: WorkflowContext;
+  step: StepTools;
+};
+
+export type NodeExecutor<TData = Record<string, unknown>> = (
+  params: NodeExecutionParams<TData>,
+) => Promise<WorkflowContext>;
---
+++ src/inngest/utils.ts
@@ -0,0 +1,45 @@
+import { Connection, Node } from "@/generated/prisma/client";
+import toposort from "toposort";
+
+export const topologiacalSort = (
+  nodes: Node[],
+  connections: Connection[],
+): Node[] => {
+  if (connections.length === 0) return nodes;
+
+  const edges: [string, string][] = connections.map((connection) => [
+    connection.fromNodeId,
+    connection.toNodeId,
+  ]);
+
+  // Connection as self-edges
+  const connectedNodeIds = new Set<string>();
+  for (let connection of connections) {
+    connectedNodeIds.add(connection.fromNodeId);
+    connectedNodeIds.add(connection.toNodeId);
+  }
+
+  for (let node of nodes) {
+    if (!connectedNodeIds.has(node.id)) {
+      edges.push([node.id, node.id]);
+    }
+  }
+
+  let sortedNodeIds: string[];
+  try {
+    sortedNodeIds = toposort(edges);
+
+    // Remove duplicated (self-edges)
+    sortedNodeIds = [...new Set(sortedNodeIds)];
+  } catch (error) {
+    if (error instanceof Error && error.message.includes("Cyclic")) {
+      throw new Error("Cyclic dependency detected in workflow nodes");
+    }
+
+    throw error;
+  }
+
+  // Map sorted node IDs back to nodes
+  const nodeMap = new Map(nodes.map((node) => [node.id, node]));
+  return sortedNodeIds.map((nodeId) => nodeMap.get(nodeId)!).filter(Boolean);
+};
